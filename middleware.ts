fc8e97e33f42f1584118fdc89235bb2808e3b072
Olawill <henry.williams658@gmail.com>
2024-07-29 18:04:23 -0600

    Refactor middleware error

--- middleware.ts
+++ middleware.ts
@@ -18,13 +18,15 @@ export default auth((req) => {
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  if (isApiAuthRoute) return null;
+  // if (isApiAuthRoute) return null;
+  if (isApiAuthRoute) return;
 
   if (isAuthRoute) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
-    return null;
+    // return null;
+    return;
   }
 
   if (!isLoggedIn && !isPublicRoute) {
@@ -42,7 +44,42 @@ export default auth((req) => {
   }
 
   // return null
+  // return;
 });
+
+// export async function middleware(req: Request) {
+//   const url = new URL(req.url);
+//   const session = await getServerSession(req, authConfig);
+
+//   const isLoggedIn = !!session;
+
+//   const isApiAuthRoute = url.pathname.startsWith(apiAuthPrefix);
+//   const isPublicRoute = publicRoutes.includes(url.pathname);
+//   const isAuthRoute = authRoutes.includes(url.pathname);
+
+//   if (isApiAuthRoute) return NextResponse.next();
+
+//   if (isAuthRoute) {
+//     if (isLoggedIn) {
+//       return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, url));
+//     }
+//     return NextResponse.next();
+//   }
+
+//   if (!isLoggedIn && !isPublicRoute) {
+//     let callbackUrl = url.pathname;
+
+//     if (url.search) {
+//       callbackUrl += url.search;
+//     }
+
+//     const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+
+//     return NextResponse.redirect(new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, url));
+//   }
+
+//   return NextResponse.next();
+// }
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
